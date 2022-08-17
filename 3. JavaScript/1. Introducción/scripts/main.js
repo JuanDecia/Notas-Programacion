104ddca78951961c825079aa8a603d7946ab9489
JuanDecia <Juan.decia@gmail.com>
2022-08-17 14:57:14 -0300

    JS
    Introducción a JavaScript.

---
+++ 3. JavaScript/1. Introducción/scripts/main.js
@@ -0,0 +1,11 @@
+function sumaValor() {
+    let a = 1;
+
+    a++; // Suma a en 1: a = 1 (++) a = 1 || RESULTADO: a = 2 //
+
+    let b = 1;
+
+    console.log(`El valor de a es: ${a}. `);
+}
+
+sumaValor();
