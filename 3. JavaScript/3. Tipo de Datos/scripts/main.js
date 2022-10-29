155148c72d8cfeb07f39a0103aa5f1d12812674c
JuanDecia <Juan.decia@gmail.com>
2022-10-29 11:30:46 -0300

    Tipo de Datos
    Se actualiza notas.

--- 3. JavaScript/3. Tipo de Datos/scripts/main.js
+++ 3. JavaScript/3. Tipo de Datos/scripts/main.js
@@ -1,54 +1,65 @@
+let contenedorResultado = document.getElementsByClassName('cont-resultado');
+
 // SECCION INT
-let numero = 5;
-document.getElementById('cont-int').innerHTML  = `${numero}`;
-console.log(`VARIABLE TIPO INT: `)
-console.log(`Variable de tipo de dato int: numero = ${numero}`);
+let numeroCinco = 5;
+contenedorResultado[0].innerHTML += `Variable '${numeroCinco}' es de tipo '${typeof numeroCinco}'.`;
+console.log(`%c#################### Dato INT: `, "color: #71B1C1");
+console.log(`Variable '${numeroCinco}' es de tipo '${typeof numeroCinco}'.`);
 
 console.log('');
 
 // SECCION FLOAT
 let numeroFloat = 0.5;
-document.getElementById('cont-float').innerHTML = `${numeroFloat}`;
-console.log(`VARIABLE TIPO FLOAT: `)
-console.log(`Variable de tipo de dato float: numeroFloat = ${numeroFloat}`);
+contenedorResultado[1].innerHTML += `Variable '${numeroFloat}' es de tipo '${typeof numeroFloat}'.`;
+console.log(`%c#################### Dato FLOAT: `, "color: #71B1C1");
+console.log(`Variable '${numeroFloat}' es de tipo '${typeof numeroFloat}'.`);
 
 console.log('');
 
 // SECCION STRING
 let tipoString = 'Soy una cadena de caracteres de tipo string';
-document.getElementById('cont-string').innerHTML = `${tipoString}`;
-console.log(`VARIABLE TIPO STRING: `);
-console.log(`La siguiente cadena de caracteres es un dato de tipo string: '${tipoString}'.`);
+contenedorResultado[2].innerHTML += `Variable 'tipoString' es de tipo '${typeof tipoString}'.`;
+console.log(`%c#################### Dato STRING: `, "color: #71B1C1");
+console.log(`Variable 'tipoString' es de tipo '${typeof tipoString}'.`);
 
 console.log('');
 
 // SECCION LIST & ARRAY (Listas o arreglos)
 let colores = ["Rojo", "Blanco", "Negro"];
-let arreglo = document.getElementById('cont-array');
+contenedorResultado[3].innerHTML += `Variable 'colores' es de tipo: 'array'.`;
 
-console.log(`VARIABLE TIPO LISTA: `);
-for (let color in colores) {
-    arreglo.innerHTML += `${colores[color]} `;
-    console.log(`${colores[color]}`)
-}
+console.log(`%c#################### Dato ARRAY: `, "color: #71B1C1");
+console.log(colores);
 
 console.log('');
 
 // SECCION OBJETO
-let person = {Altura: "1.60", ColorDeOjos: "cafe"};
-document.getElementById('cont-objeto').innerHTML += `La altura de la persona objeto es: '${person.Altura}'`;
-console.log(`VARIABLE OBJETO: `);
-console.log(person);
+let objetoPersona = {
+    Altura: "1.60", 
+    ColorDeOjos: "cafe",
+};
+
+contenedorResultado[4].innerHTML += `Variable 'objetoPersona' es de tipo: '${typeof objetoPersona}'.`;
+console.log(`%c#################### Dato OBJETO: `, "color: #71B1C1");
+console.log(objetoPersona);
 
 console.log('')
 
 // SECCIÓN BOOLEAN
 let estaActivo = true;
-document.getElementById('cont-bool').innerHTML += `El estado del usuario es: '${estaActivo}'.`;
-console.log(`VARIABLE DE TIPO BOOLEAN: `);
-console.log(estaActivo);
+contenedorResultado[5].innerHTML += `Variable 'estaActivo' es de tipo: '${typeof estaActivo}'.`;
+console.log(`%c#################### Dato OBJETO: `, "color: #71B1C1");
+console.log(`Variable 'estaActivo' es de tipo: '${typeof estaActivo}'.`);
 
 console.log('');
+
+// IS NAN
+let numeroString = 'numeroCinco';
+contenedorResultado[6].innerHTML += `Valor de la variable 'numeroString' en los parámetros de parseInt: ${parseInt(numeroString)}.`;
+console.log(`%c#################### Dato NAN: `, "color: #71B1C1");
+console.log(`Valor de la variable 'numeroString' en los parámetros de parseInt: ${parseInt(numeroString)}.`);
+
+console.log("");
 
 // DATOS COMPUESTOS
 let persona;
@@ -56,7 +67,7 @@ let persona;
 persona = {
     nombre: "Luis",
     apellido: "Torres",
-    telefono: ["12345"],
+    telefono: ["12345", "67891"],
     direccion: {
         estado: "Principal",
         ciudad: "Dorado",
@@ -65,99 +76,125 @@ persona = {
     }
 }
 
-document.getElementById('nombre').innerHTML += `${persona.nombre}`;
-document.getElementById('apellido').innerHTML += `${persona.apellido}`;
-document.getElementById('telefono').innerHTML += `${persona.telefono}`;
-document.getElementById('estado').innerHTML += `${persona.direccion.estado}`;
-document.getElementById('ciudad').innerHTML += `${persona.direccion.ciudad}`;
-document.getElementById('calle').innerHTML += `${persona.direccion.calle}`;
-document.getElementById('casa').innerHTML += `${persona.direccion.casa}`;
+persona.edad = 20;
+let clavesObjeto = Object.keys(persona);
 
+console.log(`%c#################### Datos Compuestos: `, "color: #71B1C1");
 console.log(`VARIABLE CON DATOS COMPUESTOS: `);
 console.log(persona);
+
+console.log("");
+
+console.log(`Dato del primer elemento de teléfonos: '${persona.telefono[0]}'.`);
+contenedorResultado[7].innerHTML += `Dato del primer elemento de teléfonos: '${persona.telefono[0]}'.`;
+
+console.log(`Elementos en el arreglo: ${persona.telefono.length}.`);
+contenedorResultado[8].innerHTML += `Elementos en el arreglo: ${persona.telefono.length}.`;
+
+console.log("");
+
+console.log(`Accediendo al valor de estado de la propiedad de tipo objeto llamada 'dirección': ${persona.direccion.estado}.`);
+contenedorResultado[9].innerHTML += `Accediendo al valor de estado de la propiedad de tipo objeto llamada 'dirección': ${persona.direccion.estado}.`;
+
+console.log(`Ahora la variable persona tiene una propiedad ${clavesObjeto[4]}. El valor de esa propiedad es: '${persona.edad}'.`);
+contenedorResultado[10].innerHTML += `Ahora la variable persona tiene una propiedad '${clavesObjeto[4]}'. El valor de esa propiedad es: '${persona.edad}'.`;
 
 console.log('');
 
 // CAMBIAR TIPO DE DATO CON PARSEINT()
+console.log(`%c#################### CAMBIAR TIPO DE DATO: `, "color: #71B1C1");
 
 let numeroTipoLetra = '25';
-let numeroLetra = '19';
-document.getElementById('tipoLetra').innerHTML += `${numeroTipoLetra} es tipo ${typeof(numeroTipoLetra)} <br /> <br />`;
+
+console.log(`Variable 'numeroTipoLetra' tiene valor: ${numeroTipoLetra} y es de tipo ${typeof numeroTipoLetra}.`);
+contenedorResultado[11].innerHTML += `${numeroTipoLetra} es tipo '${typeof(numeroTipoLetra)}'.`;
 
 let numeroTipoNumero = parseInt(numeroTipoLetra);
-const edad = Number(numeroLetra);
-document.getElementById('tipoNumero').innerHTML += `${numeroTipoNumero} es tipo ${typeof(numeroTipoNumero)}`;
-
-console.log(`CAMBIAR TIPOS DE DATOS: `);
-console.log(`${numeroTipoLetra} es de tipo: '${typeof numeroTipoLetra}'.`);
-console.log(`${numeroTipoNumero} es de tipo '${typeof numeroTipoNumero}'.`);
-console.log(typeof edad);
+console.log(`Variable 'numeroTipoLetra' fue ingresada al method 'parseInt()'. Ahora, ${numeroTipoNumero} es de tipo '${typeof numeroTipoNumero}'.`);
+contenedorResultado[12].innerHTML += `Variable 'numeroTipoLetra' fue ingresada al method 'parseInt()'. Ahora, ${numeroTipoNumero} es de tipo '${typeof numeroTipoNumero}'.`;
 
 console.log('');
 
 // UNDEFINED & NULL
+console.log(`%c#################### NULL & UNDEFINED: `, "color: #71B1C1");
+
 let variableVacia;
 let variableNull = null;
 
-document.getElementById('cont-undefined').innerHTML += `variableVacia es de tipo: <b>${variableVacia}</b>. <br><br>`;
-document.getElementById('cont-null').innerHTML += `variableNull es de tipo <b>${variableNull}</b>.`
+console.log(`Variable vacía: ${variableVacia}.`);
+contenedorResultado[13].innerHTML += `Variable vacía: ${variableVacia}.`
 
-console.log(`VARIABLES TIPO UNDEFINED Y NULL: `);
-console.log(variableVacia);
-console.log(variableNull);
+console.log("");
+
+console.log(`Variable de tipo: ${variableNull}.`);
+contenedorResultado[14].innerHTML += `Variable de tipo: ${variableNull}.`;
 
 console.log('');
 
 // TYPEOF
-let verTipoDato = 'Juan';
-document.getElementById('cont-typeof').innerHTML += `El tipo de dato de esta variable es: ${typeof verTipoDato}`
+console.log(`%c#################### TYPEOF: `, "color: #71B1C1");
 
-console.log('METHOD TYPEOF: ');
-console.log(typeof verTipoDato);
+let elementos = ['Fuego', 'Tierra', 'Agua', 'Aire'];
+
+console.log(`Variable 'elementos' es de tipo: ${typeof elementos}.`);
+contenedorResultado[15].innerHTML += `Variable 'elementos' es de tipo: ${typeof elementos}.`;
 
 console.log('');
 
 // COPIAR VALORES 
-let nombreAlumno = 'Juan';
-let alumnoRepetido = nombreAlumno;
+console.log(`%c#################### COPIANDO VALORES: `, "color: #71B1C1");
+let color = 'Blanco';
+let copiaColor = color;
 
-document.getElementById('cont-VariableNueva').innerHTML += `El valor de nombreAlumno es ${nombreAlumno}.`
-document.getElementById('cont-variableRepetida').innerHTML += `El valor de alumnoRepetido es ${alumnoRepetido}.`
+console.log(`Valor de la variable 'color': ${color}.`);
+contenedorResultado[16].innerHTML += `Valor de la variable 'color': ${color}.`;
 
-console.log('COPIAR DATOS DE VARIABLES A OTRA VARIABLE: ');
-console.log(`El valor de nombreAlumno es '${nombreAlumno}'.`);
-console.log(`El valor de alumnoRepetido es '${alumnoRepetido}'.`);
+console.log(`Valor de la variable 'copiaColor': ${copiaColor}.`);
+contenedorResultado[17].innerHTML += `Valor de la variable 'copiaColor': ${copiaColor}.`;
 
 console.log('');
 
 // ESTRUCTURA: VARIABLES COMPLEJAS
+console.log(`%c#################### ESTRUCTURA VARIABLE COMPLEJAS: `, "color: #71B1C1");
+
 let personas;
 personas = [
     {
         nombre: "Luis",
     }
-]
+];
 
-let personaUno = document.getElementsByClassName('datoCompuesto');
-personaUno[0].innerHTML += `${personas[0].nombre}`;
-console.log(personas[0].nombre);
+console.log(personas);
+console.log(`Valor en el arreglo: ${personas[0].nombre}.`);
 
-personas.push( // Agregamos al arrego con el metodo push: {nombre: "Marcos", edad: "10"} //
+// Agregamos al arrego con el metodo push: {nombre: "Marcos", edad: "10"} //
+personas.push( 
     {
         nombre: "Marcos",
         edad: 10,
     }
 )
 
-let personaDos = document.getElementsByClassName('datoCompuesto');
-personaDos[1].innerHTML += `${personas[0].nombre}, ${personas[1].nombre} ${personas[1].edad},`;
-console.log(personas); // Tenemos una lista con 2 objetos
+// Agregamos al objeto donde se encuentra 'Marcos' otro dato llamado amigos con los valores de roberto y ana
+personas[1].amigos = ['Roberto', 'Ana']; 
 
-personas[1].amigos = ['Roberto', 'Ana']; // Agregamos al objeto donde se encuentra 'Marcos' otro dato llamado amigos con los valores de roberto y ana //
-console.log(personas); // {nombre: Luis}, {nombre: Marcos, edad: 10, amigos: Roberto, Ana} //
+console.log("");
 
-let usuarios = personas; // Asignamos a otra variable el contenido de "personas".
-console.log(usuarios); // Imprime lo mismo que "personas".
+console.log("Estructura del arreglo de personas luego de la declaración de 'amigos'.");
+console.log(personas);
 
-usuarios[1].amigos[0] = 'Pedro'; // Cambiamos 'Pedro' por 'Marcos' solo en la variable usuarios.
+console.log("");
+
+// Asignamos a otra variable el contenido de "personas".
+let usuarios = personas;
+
+console.log("Resultado de la copia de personas en una variable llamada 'usuarios'.");
+console.log(usuarios);
+
+console.log("");
+
+// Cambiamos 'Pedro' por 'Marcos' solo en la variable usuarios.
+usuarios[1].amigos[0] = 'Pedro'; 
+
+console.log("Cambiamos pedro por marcos en la variable usuarios: ");
 console.log(usuarios[1].amigos);
