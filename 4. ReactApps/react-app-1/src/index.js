12cad7e8aa06812900cb2ce914cfd5bcb1502525
JuanDecia <Juan.decia@gmail.com>
2022-12-08 17:54:44 -0300

    Hola mundo
    Primer hola mundo en React

--- 4. ReactApps/react-app-1/src/index.js
+++ 4. ReactApps/react-app-1/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM  from 'react-dom/client'
+
+/* 
+    Forma #1
+        const rootElement = document.getElementById('root');
+        ReactDOM.createRoot(rootElement);
+*/
+
+/* 
+    Forma #2
+        ReactDOM.createRoot(document.getElementById('root'));
+*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const html = ReactDOM.createRoot(document.getElementById('html'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<h1>Hello World!</h1>);
+html.render(<h2>Hola React!</h2>);
