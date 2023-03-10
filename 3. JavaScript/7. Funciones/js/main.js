a0d487cdb874a2ee5b3c1a3cd85b84cf56cdc33e
JuanDecia <Juan.decia@gmail.com>
2023-03-10 15:39:26 -0300

    Funciones
    Corrección de descripciones y resultados en el código y DOM.

--- 3. JavaScript/7. Funciones/js/main.js
+++ 3. JavaScript/7. Funciones/js/main.js
@@ -8,11 +8,12 @@ console.log(`Ejemplo sin utilizar return: `);
 
 const cuentaSumar = (a, b) => {
     console.log(`La suma entre los dos numeros es: ${a + b}.`);
-    return a + b;
 }
 
+// Mostramos Resultados
 cuentaSumar(5, 10);
-contenedorResultado[0].innerHTML = `El resultado de la suma es ${cuentaSumar(5, 2)}.`
+contenedorResultado[0].innerHTML = `El resultado de la suma es ${cuentaSumar(5, 2)}.`;
+
 console.log(" ");
 
 // #################### EJEMPLO FUNCION C/RETURN #################### //
@@ -21,6 +22,7 @@ console.log("%c#################### EJEMPLO FUNCION C/RETURN: ", "color: #71B1C1
 function sumaResultado(a, b) {
     return a + b;
 }
+
 console.log(`El resultado de la suma es: ${sumaResultado(22, 43)}.`);
 console.log(" ");
 
