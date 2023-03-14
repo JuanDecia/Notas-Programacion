430454ceaab4bf7da21503941b4d70d0932c0c31
JuanDecia <Juan.decia@gmail.com>
2023-03-14 11:32:19 -0300

    Funciones
    Se corrigen descripciones, código y se corrigió código viejo a nuevo.

--- 3. JavaScript/7. Funciones/js/main.js
+++ 3. JavaScript/7. Funciones/js/main.js
@@ -4,37 +4,36 @@ const contenedorResultado = document.getElementsByClassName('cont-resultado');
 // #################### EJEMPLO FUNCION S/RETURN #################### //
 console.log("%c#################### EJEMPLO FUNCION S/RETURN: ", "color: #71B1C1");
 
-console.log(`Ejemplo sin utilizar return: `);
-
 const cuentaSumar = (a, b) => {
     console.log(`La suma entre los dos numeros es: ${a + b}.`);
 }
 
 // Mostramos Resultados
-cuentaSumar(5, 10);
 contenedorResultado[0].innerHTML = `El resultado de la suma es ${cuentaSumar(5, 2)}.`;
+
+cuentaSumar(5); // Error por falta de parámetro.
 
 console.log(" ");
 
 // #################### EJEMPLO FUNCION C/RETURN #################### //
 console.log("%c#################### EJEMPLO FUNCION C/RETURN: ", "color: #71B1C1");
 
-function sumaResultado(a, b) {
-    return a + b;
+const cuentaMultiplicar = (a, b) => {
+    return a * b 
+};
+
+
+// Mostramos resultados
+console.log(`El resultado de la multiplicación es: ${cuentaMultiplicar(5, 2)}.`);
+contenedorResultado[1].innerHTML = `El resultado de la suma es ${cuentaMultiplicar(5, 2)}.`;
+
+console.log(" ");
+
+// #################### PARÁMETRO POR DEFECTO #################### //
+
+const multiplicarPorDefecto = (a, b = 1) => {
+    return a * b
 }
-
-console.log(`El resultado de la suma es: ${sumaResultado(22, 43)}.`);
-console.log(" ");
-
-// #################### EJEMPLO INSTANCIANDO FUNCION #################### //
-console.log("%c#################### EJEMPLO INSTANCIANDO FUNCION: ", "color: #71B1C1");
-
-let resultadoA = sumaResultado(22, 54);
-console.log(`El resultado de la variable 'resultadoA' es: ${resultadoA}.`);
-
-let resultadoB = resultadoA;
-console.log(`La variable 'resultadoB' contiene el valor de 'resultadoA' que es: ${resultadoB}.`);
-console.log(" ");
 
 // #################### EJEMPLO SELF INVOKING #################### //
 console.log("%c#################### EJEMPLO SELF INVOKING: ", "color: #71B1C1");
