6b024bfbafb4c825c6fa42661febca0e2cc7c387
JuanDecia <Juan.decia@gmail.com>
2023-08-24 17:13:40 -0300

    js externo
    Se crea la página y el ejemplo práctico para el concepto de utilizar JavaScript en una página externa.

--- 2. JavaScript/1. Introducción/scripts/main.js
+++ 2. JavaScript/1. Introducción/scripts/main.js
@@ -1,3 +1,44 @@
+// Página JS Externa
+
+const getInput = document.getElementById('nombre');
+const mostrarResultado = document.querySelector('.resultado');
+
+const renderNombre = () => {
+    let nombrePersona = getInput.value.trim();
+    let contieneNumeros = false;
+
+    for (let i = 0; i < nombrePersona.length; i++) {
+        if (!isNaN(nombrePersona[i])) {
+            contieneNumeros = true;
+            break;
+        }
+    }
+
+    if (nombrePersona == '') {
+        mostrarResultado.style.color = 'red';
+        mostrarResultado.innerHTML = `No ha ingresado un nombre válido, intente nuevamente por favor.`;
+        mostrarResultado.style.animation = 'none';
+        void mostrarResultado.offsetWidth; // Reinicia la animación
+        mostrarResultado.style.animation = 'fade 1.3s ease-in-out';
+    }
+    else if (contieneNumeros) {
+        mostrarResultado.style.color = 'red';
+        mostrarResultado.innerHTML = `No está permitido ingresar números en el campo. Por favor, intente nuevamente utilizando solo letras.`;
+        mostrarResultado.style.animation = 'none';
+        void mostrarResultado.offsetWidth; // Reinicia la animación
+        mostrarResultado.style.animation = 'fade 1.3s ease-in-out';
+    }
+    else {
+        mostrarResultado.style.color = 'black';
+        mostrarResultado.innerHTML = `Su nombre es: '${nombrePersona}'`;
+        mostrarResultado.style.animation = 'none';
+        void mostrarResultado.offsetWidth; // Reinicia la animación
+        mostrarResultado.style.animation = 'fade 1.3s ease-in-out';
+    }
+}
+
+// FIN PÁGINA JS EXTERNA.
+
 function sumaValor() {
     let a = 1;
 
